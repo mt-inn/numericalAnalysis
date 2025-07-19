fa8302322788cd3ba0e563e43ae5f4debd45a43e
bakedMochi <ryua4ymnk@gmail.com>
2025-07-19 11:47:23 +0900

    Update invert.cpp

--- invert.cpp
+++ invert.cpp
@@ -4,12 +4,26 @@
 
 void main(void)
 {
+    /* 
+    変数を定義
+    a: 入力する行列
+    inv: 逆行列になる場所
+    p, aik: 計算中の係数を格納
+    i,j,k,l,n: 繰り返し処理のカウンター。可読性のために別々に用意
+    */
     double a[10][10], inv[10][10], p, aik;
-    int i, j, k, l, n;
+    int i=0;, j, k, l, n;
     printf("元数を入力:\n");
     scanf("%d", &n);
     printf("定数を入力:\n");
-    //計算する行列を1文字ずつ入力
+    /*
+    計算する行列を1文字ずつ入力
+    下のような入力順
+      | 1 2 3 |
+      | 4 5 6 |
+      | 7 8 9 |
+    
+    */
     for (i = 0;i < n;i++) {
         for (j = 0;j < n;j++) {
             scanf("%lf", &a[i][j]);
@@ -37,11 +51,9 @@ void main(void)
         }
 
         for (i = 0;i < n;i++) {
-            //i列目、かつk行目じゃない部分について計算
+            //i列目かつk行目じゃない部分について計算
             if (i != k) {
-                // each columns
                 aik = a[i][k];
-                //サンプルプログラムからの変更点。
                 for (j = 0; j < n; j++) {
                     a[i][j] = a[i][j] - aik * a[k][j];
                     inv[i][j] = inv[i][j] - aik * inv[k][j];
@@ -56,6 +68,7 @@ void main(void)
             }
             printf("|\n");
         }
+        //計算が終わった場所を明示
         printf("---------------------------------\n");
     }
     printf("逆行列\n");
@@ -67,11 +80,3 @@ void main(void)
         printf("|\n");
     }
 }
-/*
-EXPECTED ANSWER
-
-|  1  -2  0  |
-|  1  1  -1  |
-|  -5  5  2  |
-
-*/
